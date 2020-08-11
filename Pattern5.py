0e92ec85e0a9d9658c7a30ae44a2425895a5ed65
Muskan2917 <64564596+Muskan2917@users.noreply.github.com>
2020-08-11 10:52:35 +0530

    Create Pattern5.py

---
+++ Pattern5.py
@@ -0,0 +1,29 @@
+"""
+A Program To Print The Following Pattern.
+
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+
+"""
+#Program code to print above pattern
+
+for x in range(1,6):
+    for y in range(1,6):
+        print(y,end=" ")
+    print()
+
+
+"""
+OUTPUT:
+
+C:\python3.7\python.exe C:/Users/lenovo/PycharmProjects/1frog/Python_Pattern/Pattern5.py
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+
+"""
