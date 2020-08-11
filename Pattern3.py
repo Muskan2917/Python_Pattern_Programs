287dad58ec2e6348dc47471faa80c86be9511c16
Muskan2917 <64564596+Muskan2917@users.noreply.github.com>
2020-08-11 10:43:05 +0530

    Create Pattern3.py

---
+++ Pattern3.py
@@ -0,0 +1,27 @@
+"""
+A Program To Print The Following Star(*) Pattern Using While Loop
+
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+
+"""
+#Program code to print above pattern
+for x in range(1,6):
+    for y in range(1,6):
+        print("*",end=" ")
+    print()
+
+"""
+OUTPUT:
+
+C:\python3.7\python.exe C:/Users/lenovo/PycharmProjects/1frog/Python_Pattern/Pattern3.py
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+
+"""
