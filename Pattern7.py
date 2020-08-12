ee370309d4be474a17c3d9e366a9a524842224cc
Muskan2917 <64564596+Muskan2917@users.noreply.github.com>
2020-08-12 20:04:53 +0530

    Create Pattern7.py

---
+++ Pattern7.py
@@ -0,0 +1,30 @@
+"""
+A Program To Print The Following Pattern.
+
+A B C D E 
+A B C D E 
+A B C D E 
+A B C D E 
+A B C D E 
+
+"""
+#Program code to print above pattern
+
+for x in range(1,6):
+    for y in range(65,70):
+        print(chr(y),end=" ")
+    print()
+
+"""
+OUTPUT:
+
+C:\python3.7\python.exe C:/Users/lenovo/PycharmProjects/1frog/Python_Pattern/Pattern7.py
+
+A B C D E 
+A B C D E 
+A B C D E 
+A B C D E 
+A B C D E 
+
+
+"""
