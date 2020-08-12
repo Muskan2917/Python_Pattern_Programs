e280b798c78f0c1d6a9f6e20d3d4dcdd0fb3533a
Muskan2917 <64564596+Muskan2917@users.noreply.github.com>
2020-08-12 20:10:36 +0530

    Create Pattern8.py

---
+++ Pattern8.py
@@ -0,0 +1,28 @@
+"""
+A Program To Print The Following Pattern.
+
+E E E E E 
+D D D D D 
+C C C C C 
+B B B B B 
+A A A A A 
+
+"""
+#Program code to print above pattern
+
+for x in range(69,64,-1):
+    for y in range(1,6):
+        print(chr(x),end=" ")
+    print()
+
+"""
+OUTPUT:
+
+C:\python3.7\python.exe C:/Users/lenovo/PycharmProjects/1frog/Python8.py
+E E E E E 
+D D D D D 
+C C C C C 
+B B B B B 
+A A A A A 
+
+"""
