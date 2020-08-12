844a5fbca170b086a13ce7a3066eee1bfd824a02
Muskan2917 <64564596+Muskan2917@users.noreply.github.com>
2020-08-12 20:12:07 +0530

    Create Pattern9.py

---
+++ Pattern9.py
@@ -0,0 +1,28 @@
+"""
+A Program To Print The Following Pattern.
+
+E D C B A 
+E D C B A 
+E D C B A 
+E D C B A 
+E D C B A 
+
+"""
+#Program code to print above pattern
+
+for x in range(1,6):
+    for y in range(69,64,-1):
+        print(chr(y),end=" ")
+    print()
+    
+"""
+OUTPUT:
+
+C:\python3.7\python.exe C:/Users/lenovo/PycharmProjects/1frog/Pattern9.py
+E D C B A 
+E D C B A 
+E D C B A 
+E D C B A 
+E D C B A 
+
+"""
