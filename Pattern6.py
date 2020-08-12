1a8fa63adc27b2c635dbee4915e4c7a947e7583f
Muskan2917 <64564596+Muskan2917@users.noreply.github.com>
2020-08-12 20:03:23 +0530

    Create Pattern6.py

---
+++ Pattern6.py
@@ -0,0 +1,30 @@
+"""
+A Program To Print The Following Pattern.
+
+A A A A A 
+B B B B B 
+C C C C C 
+D D D D D 
+E E E E E 
+
+"""
+#Program code to print above pattern
+
+for x in range(65,70):
+    for y in range(1,6):
+        print(chr(x),end=" ")
+    print()
+
+"""
+OUTPUT:
+
+C:\python3.7\python.exe C:/Users/lenovo/PycharmProjects/1frog/Python_Pattern/Pattern6.py
+
+A A A A A 
+B B B B B 
+C C C C C 
+D D D D D 
+E E E E E 
+
+
+"""
