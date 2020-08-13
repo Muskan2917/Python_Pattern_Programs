fc7e9766c16048fa0f71519b33b3ff88ca0e1053
Muskan2917 <64564596+Muskan2917@users.noreply.github.com>
2020-08-13 22:54:42 +0530

    Create Pattern11.py

---
+++ Pattern11.py
@@ -0,0 +1,27 @@
+"""
+A Program To Print The Following Pattern.
+
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+
+"""
+#Program code to print above pattern
+
+for x in range(1,6):
+    for y in range(1,x+1):
+        print(x,end=" ")
+    print()
+"""
+OUTPUT:
+
+C:\python3.7\python.exe C:/Users/lenovo/PycharmProjects/1frog/Python_Pattern/Pattern11.py
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+
+"""
