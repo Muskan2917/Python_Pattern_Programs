cf60d758dedc301446714faaf4ec6cd39965caf1
Muskan2917 <64564596+Muskan2917@users.noreply.github.com>
2020-08-13 22:52:49 +0530

    Create Pattern10.py

---
+++ Pattern10.py
@@ -0,0 +1,28 @@
+"""
+A Program To Print The Following Pattern.
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+
+"""
+#Program code to print above pattern
+
+for x in range(1,6):
+    for y in range(1,x+1):
+        print("*",end=" ")
+    print()
+
+"""
+OUTPUT:
+
+C:\python3.7\python.exe C:/Users/lenovo/PycharmProjects/1frog/Python_Pattern/Pattern10.py
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+
+"""
